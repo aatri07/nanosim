8c176969b4eeedbca5c7144906fa1c0a31a59b82
aatri07 <aatripathy.07@gmail.com>
2026-02-20 16:19:52 -0600

    Fixed error in main.cpp

---
+++ build/CMakeFiles/NanoSim.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NanoSim.dir/src/main.cpp.o"
+  "CMakeFiles/NanoSim.dir/src/main.cpp.o.d"
+  "NanoSim"
+  "NanoSim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NanoSim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
